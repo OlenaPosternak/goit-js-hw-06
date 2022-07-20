e1356d3b678399251fad17f56615eb18ff11bcf5
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-07-20 23:27:30 +0300

    task 2

--- js/task-01.js
+++ js/task-01.js
@@ -19,6 +19,14 @@ const arr = nameOfCategories.forEach(function(name){
     console.log(`Elements`,name.nextElementSibling.childElementCount )
 });
 
+// Варіант 2 (через дітей)
 
+// const allChildren = nameOfFullList.children;
+// //Отримуємо псевдомасив і перетворюємо на масив
+// const arr= Array.from(allChildren);
 
+// const result = arr.forEach(function(name){
+//     console.log(`Category:`, name.firstElementChild.textContent);
+//     console.log(`Elements`,name.lastElementChild.childElementCount);
 
+// })
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,31 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+
+// Перебираємо масив та на кожній ітерації створюємо новий li (також додаємо класс і текстовий контент)
+
+const creatListElements = ingredients.map(function (element){
+
+    const el=document.createElement(`li`);
+    console.log(el);
+    
+    el.classList.add(`item`)
+    el.textContent=element;
+
+    return el;
+
+});
+
+// прикріпляємо наш список до батьківського елементу
+const allIngridients = document.querySelector(`#ingredients`);
+allIngridients.append(...creatListElements);
+
