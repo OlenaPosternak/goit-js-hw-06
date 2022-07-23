2c0f818ffc622c3cc2766b417c923f5102601292
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-07-23 15:55:14 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
+// Создай функцию createBoxes(amount), которая принимает один параметр - число.
+//  Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
+
+// Размеры самого первого <div> - 30px на 30px.
+// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+// Все элементы должены иметь случайный цвет фона в формате HEX. 
+// Используй готовую функцию getRandomHexColor для получения цвета.
+
+
+const inputForm = document.querySelector(`input`);
+inputForm.addEventListener(`input`, onInputChanges);
+
+
+function onInputChanges (){
+    let newValue = Number(inputForm.value);
+    createBoxes(newValue);
+};
+
+
+function createBoxes (amount){
+    console.log (amount);
+}
+
+console.log(createBoxes());
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
